e993e26c6c69fc3c42343e4db677225d23944164
lindori <78268706+lindori@users.noreply.github.com>
2021-04-23 18:14:26 +0200

    JS_nev

--- VD_giturlap/public_html/urlapkezeles.js
+++ VD_giturlap/public_html/urlapkezeles.js
@@ -1,7 +1,34 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+window.addEventListener("load",init);
 
-
+function ID(nev){
+    return document.getElementById(nev);
+}
+function $(nev) {
+    return document.querySelectorAll(nev);
+}
+function init() {
+    ID("kuld").addEventListener("click", validalas);
+}
+function validalas() {
+    var hiba="";
+    var urlapAdatok="";
+    var nevInput=ID("nev").value;
+    var szuro= /[A-Z]+[a-z](2,)/;
+    console.log(nevInput);
+    
+    if(nevInput.length < 3) {
+        hiba = "A név legalább 3 karakter legyen!<br>";
+    }
+   
+    else if(nevInput[0] != nevInput[0].toUpperCase())  {
+        hiba += "A név nagybetűvel kezdődjön!<br>";
+    }
+    else {
+        urlapAdatok+="Név: "+nevInput+"<br>"
+    }
+    
+    
+    $("aside section:nth-child(1) p")[0].innerHTML=hiba;
+    console.log(hiba);
+    $("aside section:nth-child(2) p")[0].innerHTML=urlapAdatok;
+}
